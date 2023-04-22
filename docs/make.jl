52ec1967853c9af7a4b5038725ef740e28436a16
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-04-22 11:09:32 +0200

    Move tutorials to examples folder and add details to the docs

--- docs/make.jl
+++ docs/make.jl
@@ -6,30 +6,6 @@ DocMeta.setdocmeta!(
     ImplicitDifferentiation, :DocTestSetup, :(using ImplicitDifferentiation); recursive=true
 )
 
-EXAMPLES_DIR_JL = joinpath(dirname(@__DIR__), "test")
-EXAMPLES_DIR_MD = joinpath(@__DIR__, "src", "examples")
-
-for file in readdir(EXAMPLES_DIR_MD)
-    if endswith(file, ".md")
-        rm(joinpath(EXAMPLES_DIR_MD, file))
-    end
-end
-
-for file in readdir(EXAMPLES_DIR_JL)
-    if (!endswith(file, ".jl")) ||
-        startswith(file, "runtests") ||
-        startswith(file, "profiling")
-        continue
-    else
-        Literate.markdown(
-            joinpath(EXAMPLES_DIR_JL, file),
-            EXAMPLES_DIR_MD;
-            documenter=true,
-            flavor=Literate.DocumenterFlavor(),
-        )
-    end
-end
-
 function markdown_title(path)
     title = "?"
     open(path, "r") do file
@@ -40,30 +16,54 @@ function markdown_title(path)
             end
         end
     end
-    return String(title)
+    return title
 end
 
-pages = [
-    "Home" => "index.md",
-    "Examples" => [
-        markdown_title(joinpath(EXAMPLES_DIR_MD, file)) => joinpath("examples", file)
-        for file in sort(readdir(EXAMPLES_DIR_MD)) if endswith(file, ".md")
-    ],
-    "API reference" => "api.md",
-]
+EXAMPLES_DIR_JL = joinpath(dirname(@__DIR__), "examples")
+EXAMPLES_DIR_MD = joinpath(@__DIR__, "src", "examples")
+
+for file in readdir(EXAMPLES_DIR_MD)
+    if endswith(file, ".md")
+        rm(joinpath(EXAMPLES_DIR_MD, file))
+    end
+end
+
+for file in readdir(EXAMPLES_DIR_JL)
+    Literate.markdown(
+        joinpath(EXAMPLES_DIR_JL, file),
+        EXAMPLES_DIR_MD;
+        documenter=true,
+        flavor=Literate.DocumenterFlavor(),
+    )
+end
+
+example_pages = Pair{String,String}[]
+for file in sort(readdir(EXAMPLES_DIR_MD))
+    if endswith(file, ".md")
+        title = markdown_title(joinpath(EXAMPLES_DIR_MD, file))
+        path = joinpath("examples", file)
+        push!(example_pages, title => path)
+    end
+end
+
+pages = ["Home" => "index.md", "API reference" => "api.md", "Examples" => example_pages]
+
+format = Documenter.HTML(;
+    prettyurls=get(ENV, "CI", "false") == "true",
+    canonical="https://gdalle.github.io/ImplicitDifferentiation.jl",
+    assets=String[],
+    edit_link=:commit,
+)
 
 makedocs(;
     modules=[ImplicitDifferentiation],
     authors="Guillaume Dalle, Mohamed Tarek and contributors",
     repo="https://github.com/gdalle/ImplicitDifferentiation.jl/blob/{commit}{path}#{line}",
     sitename="ImplicitDifferentiation.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://gdalle.github.io/ImplicitDifferentiation.jl",
-        assets=String[],
-        edit_link=:commit,
-    ),
+    format=format,
     pages=pages,
+    linkcheck=true,
+    strict=true,
 )
 
 deploydocs(; repo="github.com/gdalle/ImplicitDifferentiation.jl", devbranch="main")
