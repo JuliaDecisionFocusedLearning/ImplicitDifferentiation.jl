827f56515572da5528aac4d702278d3551186d7a
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-05-24 13:29:20 +0200

    Remove tests in favor of examplke

--- test/runtests.jl
+++ test/runtests.jl
@@ -62,20 +62,4 @@ EXAMPLES_DIR_JL = joinpath(dirname(@__DIR__), "examples")
             include(path)
         end
     end
-
-    @testset verbose = true "scalar output" begin
-        function forward(x)
-            y = sqrt(x[])
-            z = nothing
-            return y, z
-        end
-        function conditions(x, y, z)
-            c = [y^2 - x[]]
-            return c
-        end
-        implicit = ImplicitFunction(forward, conditions)
-        x = [3.14]
-        J = 0.5 / sqrt(x[])
-        @test ForwardDiff.gradient(first ∘ implicit, x)[] ≈ J
-    end
 end
