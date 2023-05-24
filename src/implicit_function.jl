1f93d66790a7c4042e718d0035ff79db4364f06b
Fredrik Bagge Carlson <baggepinnen@gmail.com>
2023-05-24 10:30:24 +0200

    Don't dualize `z` (#49)
    * Don't dualize `z`
    
    * Remove zero(z) requirement in docstring
    
    * Bump version to 0.4.3
    
    ---------
    
    Co-authored-by: Guillaume Dalle <22795598+gdalle@users.noreply.github.com>

--- src/implicit_function.jl
+++ src/implicit_function.jl
@@ -3,7 +3,7 @@
 
 Differentiable wrapper for an implicit function `x -> y(x)` whose output is defined by conditions `F(x,y(x)) = 0`.
 
-More generally, we consider functions `x -> (y(x),z(x))` and conditions `F(x,y(x),z(x)) = 0`, where `z(x)` contains additional information that _is considered constant for differentiation purposes_. Beware: the method `zero(z)` must exist.
+More generally, we consider functions `x -> (y(x),z(x))` and conditions `F(x,y(x),z(x)) = 0`, where `z(x)` contains additional information that _is considered constant for differentiation purposes_.
 
 If `x ∈ ℝⁿ` and `y ∈ ℝᵈ`, then we need as many conditions as output dimensions: `F(x,y,z) ∈ ℝᵈ`. Thanks to these conditions, we can compute the Jacobian of `y(⋅)` using the implicit function theorem:
 ```
