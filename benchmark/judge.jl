b90b326a8da96c50b8588d8cce2bcb563f1e234c
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2023-09-11 20:49:56 +0200

    Fix benchmarks judge order (#121)

--- benchmark/judge.jl
+++ benchmark/judge.jl
@@ -8,6 +8,6 @@ target = "82242b9"  # commit id
 results_baseline = benchmarkpkg(pkg, baseline; verbose=true, retune=false)
 results_target = benchmarkpkg(pkg, target; verbose=true, retune=false)
 
-judgement = judge(results_baseline, results_target, minimum)
+judgement = judge(results_target, results_baseline, minimum)
 
 export_markdown(joinpath(@__DIR__, "benchmark_judgement.md"), judgement)
