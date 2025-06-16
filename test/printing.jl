0a68e03be179d2f1119570a41551069130b4ff84
Guillaume Dalle <22795598+gdalle@users.noreply.github.com>
2025-06-16 07:57:24 +0200

    Fix coverage

--- test/printing.jl
+++ test/printing.jl
@@ -3,4 +3,5 @@ using TestItems
 @testitem "Settings" begin
     @test startswith(string(OperatorRepresentation()), "Operator")
     @test startswith(string(IterativeLinearSolver(; atol=1e-5)), "Iterative")
+    @test startswith(string(IterativeLinearSolver()), "Iterative")
 end
