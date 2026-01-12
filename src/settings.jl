5cad655e2cb30616f958594298e1cc4a29bde5d0
Maxime Bouyges <maxime.bouyges@gmail.com>
2026-01-12 16:23:38 +0100

    wip

--- src/settings.jl
+++ src/settings.jl
@@ -10,6 +10,17 @@ Specify that linear systems `Ax = b` should be solved with a direct method.
 !!! warning
     Can only be used when the `solver` and the `conditions` both output an `AbstractVector`.
 
+    Additionnaly, this solver requires a [`MatrixRepresentation`](@ref) of the matrix `A`. To do so,
+    use the `representation` keyword of the [`ImplicitFunction`](@ref) constructor :
+    ```
+    f = ImplicitFunction(
+        forward,
+        conditions;
+        solver = DirectLinearSolver(),
+        representation = MatrixRepresentation()
+    )
+    ```
+
 # See also
 
 - [`ImplicitFunction`](@ref)
